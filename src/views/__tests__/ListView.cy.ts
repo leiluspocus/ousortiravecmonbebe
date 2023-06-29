889cb86198918d078d270db921a4773a0e59d717
leiluspocus <laila.atrmouh@gmail.com>
2023-06-29 15:39:35 +0200

    :robot_face: add unit test on list view component

---
+++ src/views/__tests__/ListView.cy.ts
@@ -0,0 +1,54 @@
+import ListView from '../ListView.vue'
+
+describe('ListView', () => {
+  it('playground', () => {
+    cy.mount(ListView, {
+      props: {
+        spots: [
+          {
+            id: 1,
+            name: 'Le Bouquet Wagram',
+            address: '145 Av. De Wagram, 75017 Paris',
+            opinion: 'Une terrasse chauffée, possibilité de rentrer avec une poussette simplement',
+            lat: 48.88505,
+            lng: 2.303
+          },
+          {
+            id: 2,
+            name: 'La Felicita',
+            address: '5 Parvis Alan Turing, 75013 Paris',
+            opinion: "Des saveurs, de l'espace en masse!",
+            lat: 48.834702,
+            lng: 2.37048
+          }
+        ]
+      }
+    })
+  })
+
+  it('renders properly', () => {
+    cy.mount(ListView, {
+      props: {
+        spots: [
+          {
+            id: 1,
+            name: 'Le Bouquet Wagram',
+            address: '145 Av. De Wagram, 75017 Paris',
+            opinion: 'Une terrasse chauffée, possibilité de rentrer avec une poussette simplement',
+            lat: 48.88505,
+            lng: 2.303
+          },
+          {
+            id: 2,
+            name: 'La Felicita',
+            address: '5 Parvis Alan Turing, 75013 Paris',
+            opinion: "Des saveurs, de l'espace en masse!",
+            lat: 48.834702,
+            lng: 2.37048
+          }
+        ]
+      }
+    })
+    cy.get('h1').should('contain', 'Liste des lieux')
+  })
+})
