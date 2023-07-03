ff686c3699ac1742d5bac82e2571675620fefb82
leiluspocus <laila.atrmouh@gmail.com>
2023-07-03 18:43:30 +0200

    :sponge: useless part of template

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,76 +1,13 @@
 <script setup>
 import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
 </script>
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
   <form>
     <div class="space-y-12">
+      <h1>Proposer un lieu</h1>
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
-
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-            <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" />
-              </div>
-            </div>
-          </div>
-
-          <div class="col-span-full">
-            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
-            <div class="mt-2">
-              <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-            </div>
-            <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-          </div>
-
-          <div class="col-span-full">
-            <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
-            <div class="mt-2 flex items-center gap-x-3">
-              <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" />
-              <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
-            </div>
-          </div>
-
-          <div class="col-span-full">
-            <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
-            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-              <div class="text-center">
-                <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                  <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                    <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                  </label>
-                  <p class="pl-1">or drag and drop</p>
-                </div>
-                <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Informations sur le lieu</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">Vous pouvez proposer un nouveau lieu avec plaisir mais libre à nous de l'accepter ou non !</p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-3">
@@ -136,20 +73,20 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
       </div>
 
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Titre 2</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">Donnez nous un maximum d'informations nous permettant de savoir si le lieu est accueillant pour nos bébés.</p>
 
         <div class="mt-10 space-y-10">
           <fieldset>
-            <legend class="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
+            <legend class="text-sm font-semibold leading-6 text-gray-900">Les plus "Bébé Friendly"</legend>
             <div class="mt-6 space-y-6">
               <div class="relative flex gap-x-3">
                 <div class="flex h-6 items-center">
                   <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="comments" class="font-medium text-gray-900">Comments</label>
-                  <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                  <label for="comments" class="font-medium text-gray-900">Table à langer</label>
+                  <p class="text-gray-500">Y a-t-il de quoi changer les p'tits bouts ?</p>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -157,8 +94,8 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
                   <input id="candidates" name="candidates" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="candidates" class="font-medium text-gray-900">Candidates</label>
-                  <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
+                  <label for="candidates" class="font-medium text-gray-900">Accès pousettes</label>
+                  <p class="text-gray-500">Est-ce facile de se déplacer en poussette dans l'établissement ? </p>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -166,27 +103,9 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
                   <input id="offers" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="offers" class="font-medium text-gray-900">Offers</label>
-                  <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
+                  <label for="offers" class="font-medium text-gray-900">Staff bienveillant</label>
+                  <p class="text-gray-500">Est-ce que l'équipe est gentille avec les bébés qu'ils braillent ou qu'ils fassent risette ?</p>
                 </div>
-              </div>
-            </div>
-          </fieldset>
-          <fieldset>
-            <legend class="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-            <p class="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-            <div class="mt-6 space-y-6">
-              <div class="flex items-center gap-x-3">
-                <input id="push-everything" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                <label for="push-everything" class="block text-sm font-medium leading-6 text-gray-900">Everything</label>
-              </div>
-              <div class="flex items-center gap-x-3">
-                <input id="push-email" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                <label for="push-email" class="block text-sm font-medium leading-6 text-gray-900">Same as email</label>
-              </div>
-              <div class="flex items-center gap-x-3">
-                <input id="push-nothing" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                <label for="push-nothing" class="block text-sm font-medium leading-6 text-gray-900">No push notifications</label>
               </div>
             </div>
           </fieldset>
