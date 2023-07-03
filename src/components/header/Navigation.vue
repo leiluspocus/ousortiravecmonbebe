6fa3fda90ca015bad263c4d3173602848fd5f1ac
leiluspocus <laila.atrmouh@gmail.com>
2023-07-03 18:27:24 +0200

    :sparkles: redesign menu

--- src/components/header/Navigation.vue
+++ src/components/header/Navigation.vue
@@ -1,21 +1,12 @@
 <template>
   <Disclosure as="nav" class="bg-lightgreen-800" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl sm:px-2">
       <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-lightgreen-400 hover:bg-lightgreen-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
-        </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex flex-shrink-0 items-center">
-            <a href="/"><img class="block h-8 w-auto lg:hidden" src="/logo.png" alt="Your Company" /></a>
-            <a href="/"><img class="hidden h-8 w-auto lg:block" src="/logo.png" alt="Your Company" /></a>
+        <div class="flex flex-1 sm:justify-start">
+          <div class="flex items-center">
+            <a href="/"><img class="block h-8 w-auto lg:block" src="/logo.png" alt="Où sortir avec mon bébé ?" /></a>
           </div>
-          <div class="hidden sm:ml-6 sm:block">
+          <div class="px-4">
             <div class="flex space-x-4">
               <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-lightgreen-900 text-white' : 'text-lightgreen-300 hover:bg-lightgreen-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
             </div>
@@ -23,18 +14,11 @@
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <button type="button" class="rounded-full bg-lightgreen-800 p-1 text-lightgreen-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-lightgreen-800">
-            <span class="sr-only">View notifications</span>
             <a href="/about"><QuestionMarkCircleIcon class="h-6 w-6" aria-hidden="true" /></a>
           </button>
         </div>
       </div>
     </div>
-
-    <DisclosurePanel class="sm:hidden">
-      <div class="space-y-1 px-2 pb-3 pt-2">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-lightgreen-900 text-white' : 'text-lightgreen-300 hover:bg-lightgreen-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
-      </div>
-    </DisclosurePanel>
   </Disclosure>
 </template>
 
