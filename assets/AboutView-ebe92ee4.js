fa197a770c8d87768b93e236ef12f6197f6fbc39
leiluspocus <leiluspocus@users.noreply.github.com>
2023-07-03 10:55:14 +0000

    Deploying to gh-pages from @ leiluspocus/ousortiravecmonbebe@2f44fdd4f27d16e588563a6c57d186b7e0d92667 🚀

---
+++ assets/AboutView-ebe92ee4.js
@@ -0,0 +1,1 @@
+import{_ as e,a as t,c as o,b as s}from"./index-5304c777.js";const _={},c={class:"about"},n=s("h1",null,"A propos",-1),a=[n];function r(i,d){return t(),o("div",c,a)}const u=e(_,[["render",r]]);export{u as default};
