ea264b76f64f2ea696e20aceae43159f169edf29
leiluspocus <laila.atrmouh@gmail.com>
2023-07-21 12:49:21 +0200

    :chart_with_upwards_trend: implementing analytics with plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePluginRadar } from 'vite-plugin-radar'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-EC8BT3E4FT'
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
