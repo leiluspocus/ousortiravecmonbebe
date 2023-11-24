1af898de581ad0a3304d983b15e2f31c27613c89
leiluspocus <laila.atrmouh@gmail.com>
2023-11-24 18:20:16 +0100

    :sparkles: uninstall google analytics, try plausible

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
 
+import { createPlausible } from 'v-plausible/vue'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,8 +9,21 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const plausible = createPlausible({
+  init: {
+    domain: 'ousortiravecmonbebe.com',
+    apiHost: 'https://plausible.io',
+    trackLocalhost: false
+  },
+  settings: {
+    enableAutoOutboundTracking: true,
+    enableAutoPageviews: true
+  },
+  partytown: false
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(plausible)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { VitePluginRadar } from 'vite-plugin-radar'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: 'G-EC8BT3E4FT',
-        consentDefaults: {
-          analytics_storage: 'denied',
-          ad_storage: 'denied',
-          wait_for_update: 500
-        }
-      }
-    })
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
