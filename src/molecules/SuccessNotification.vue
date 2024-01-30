3d187a3323aa84cb712eb49635c8a0f114c62bfd
* Laila Atrmouh <leiluspocus@users.noreply.github.com>
2024-01-30 16:43:54 +0100

    :recycle: create success notification component, uniformize envoyer button (#11)

---
+++ src/molecules/SuccessNotification.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, defineProps } from 'vue'
+
+const props = defineProps({
+  msg: String,
+  success: Boolean
+})
+</script>
+
+<template>
+  <div
+    class="flex animate-bounce px-2 absolute right-0 -top-20 border-2 border-solid border-black-200 rounded-sm bg-white p-2"
+    v-show="success"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="stroke-emerald-500 mr-2 w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+<style></style>
